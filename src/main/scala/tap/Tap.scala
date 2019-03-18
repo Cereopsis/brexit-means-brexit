a6b8b43c39490511f52d3a42c2ccd8c9de41b556
Cereopsis <Cereopsis@users.noreply.github.com>
2019-03-18 10:06:45 +0000

    Add comment, minor formatting change

--- src/main/scala/tap/Tap.scala
+++ src/main/scala/tap/Tap.scala
@@ -4,28 +4,17 @@ import scalaz.zio.{UIO,ZIO,Ref}
 import scala.util.{Either,Left,Right}
 import scala.collection.mutable.Queue
 
-/**
- * A Tap adjusts the flow of requests continuously.
- *
- * The flow is adjusted based on observed failures
- * that qualify (i.e. match some user-defined predicate).
- *
- * The tap must continuously adjust the percentage of tasks it
- * lets through until it finds the maximum flow rate that satisfies 
- * the user-defined maximum error bound.
- *
- * As the service is recovering, the failure rate will drop
- * below 1%, which will cause the tap to increase flow and 
- * let more tasks through.
- * 
- */
-
 trait FlowControl {
   def rejects: UIO[Boolean]
   def fail: UIO[Unit]
   def tick: UIO[Unit]
 }
 
+/*
+ *  Maintains a sliding window of 'events', sums them and compares to threshold
+ *  Some experimentation would be needed to find an optimal window size.
+ *  More sophisticated strategies are sure to exist :-)
+ */
 final case class SimpleValve(
   window: Ref[Queue[Int]],
   threshold: Int) extends FlowControl {
@@ -67,12 +56,12 @@ object Tap {
     rejects: => E2
   ) extends Tap[E1,E2] {
     def apply[R, E >: E2 <: E1, A](effect: ZIO[R, E, A]): ZIO[R, E, A] =
-    for {
-      _ <- (flow.tick *> ZIO.fail(rejects)).whenM(flow.rejects)
-      result <- effect.foldM(
-                e => (if (qualifies(e)) flow.fail else flow.tick) *> ZIO.fail(e),
-                a => flow.tick *> UIO.succeed(a)
-              )
-    } yield result
+      for {
+        _ <- (flow.tick *> ZIO.fail(rejects)).whenM(flow.rejects)
+        result <- effect.foldM(
+                  e => (if (qualifies(e)) flow.fail else flow.tick) *> ZIO.fail(e),
+                  a => flow.tick *> UIO.succeed(a)
+                )
+      } yield result
   }
 }
